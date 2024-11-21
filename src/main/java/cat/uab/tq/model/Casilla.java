ee391805abce461525d3cc8054fbefd49fe8f31e
lucas12avl <73055673+lucas12avl@users.noreply.github.com>
2024-11-21 11:13:12 +0100

    punt de partida amb les classes i les variables que creiem necessàries amb els getters i setters (new)

---
+++ src/main/java/cat/uab/tq/model/Casilla.java
@@ -0,0 +1,53 @@
+package main.java.cat.uab.tq.model;
+/*
+  las casillas son las que saben si son una mina o no,todas tendran un numero de minas adyacentes, pero si son mina,
+   este numero nunca se representa en la interfaz. Tambien guardan la info de si el usario les ha pueto una bandera o no
+  y tambien si están reveladas
+  */
+public class Casilla {
+  private boolean tieneMina;
+  private boolean revelada;
+  private boolean tieneBandera;
+  private int minasAdjacentes;
+
+  //constructor
+  public Casilla(){
+    this.tieneMina = false;
+    this.revelada = false;
+    this.tieneBandera = false;
+    this.minasAdjacentes = 0;
+  }
+
+
+//GETTERS
+  public boolean getRevelada() {
+    return revelada;
+  }
+  public boolean getTieneMina() {
+    return tieneMina;
+  }
+  public boolean getTieneBandera() {
+    return tieneBandera;
+  }
+  public int getMinasAdjacentes() {
+    return minasAdjacentes;
+  }
+
+
+  //SETTERS
+  public void setRevelada(boolean revelada) {
+    this.revelada = revelada;
+  }
+
+  public void setTieneMina(boolean tieneMina) {
+    this.tieneMina = tieneMina;
+  }
+
+  public void setTieneBandera(boolean tieneBandera) {
+    this.tieneBandera = tieneBandera;
+  }
+
+  public void setMinasAdjacentes(int minasAdjacentes) {
+    this.minasAdjacentes = minasAdjacentes;
+  }
+}
---
+++ src/test/java/cat/uab/tq/model/CasillaTest.java
@@ -0,0 +1,9 @@
+package test.java.cat.uab.tq.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CasillaTest {
+
+}
