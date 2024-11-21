b901305e86781ffc9cda33c8bce7d920a3bcb19e
lucas12avl <73055673+lucas12avl@users.noreply.github.com>
2024-11-21 22:59:55 +0100

    punto de partida proyecto con las clases y getters/setters provisionales

---
+++ src/main/java/main.java
@@ -0,0 +1,8 @@
+public class main{
+  public static void main(String[] args) {
+
+    // llama al gestro del juego y crea un tablero con  'n' filas 'm' columas
+    // i la cantidad de minas aleatorias que queremos añadir al tablero
+
+  }
+}
