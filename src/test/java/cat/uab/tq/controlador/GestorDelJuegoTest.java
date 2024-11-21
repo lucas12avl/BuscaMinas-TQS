e6f47fa455530fc4f1ee2b4c0230499584d5d196
lucas12avl <73055673+lucas12avl@users.noreply.github.com>
2024-11-21 22:39:30 +0100

    Delete src directory

--- src/test/java/cat/uab/tq/controlador/GestorDelJuegoTest.java
+++
@@ -1,9 +0,0 @@
-package test.java.cat.uab.tq.controlador;
-
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-class GestorDelJuegoTest {
-
-}
--- src/test/java/cat/uab/tq/model/TableroTest.java
+++
@@ -1,8 +0,0 @@
-package test.java.cat.uab.tq.model;
-
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-class TableroTest {
-
-}
