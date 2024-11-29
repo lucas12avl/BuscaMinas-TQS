2ee4e78d8845cb43dac35118c14f5262a5e4be5b
alma <alva2003ro@gmail.com>
2024-11-30 00:10:40 +0100

    TEST DE MOVIMIENTO DE BANDERA MEJORADO CON MÁS CASOS --> DA ERROR --> MEJORAR IMPLEMENTACIÓN

--- src/test/java/controlador/GestorDelJuegoTest.java
+++ src/test/java/controlador/GestorDelJuegoTest.java
@@ -56,15 +56,32 @@ class GestorDelJuegoTest {
   void flagMovimientoTest() { //comprueba los movimientos de rflag y Flagging
     GestorDelJuego gestor = new GestorDelJuego(3, 3, 0);
     Tablero tablero = gestor.getTablero();
-
-    //Poner una flag en una casilla válida (no revelada)
+    //Poner una flag
     assertTrue( gestor.realizar_jugada(0, 0, 2), "Casilla (0,0) debería estar con una flag");
+
 
     //flag en una casilla no válida
     assertFalse(gestor.realizar_jugada(-1, -1, 2), "Casilla (-1,-1) no es válida");
 
+
+    //poner una flag en una casilla ya revelada
+    gestor.realizar_jugada(1, 1, 1);
+    assertFalse( gestor.realizar_jugada(1, 1, 2), "No deberia tener bandera, ya estaba revelada");
+
+
+    //PONER BANDERA EN UNA CASILLA CON BANDERA
+    assertFalse(gestor.realizar_jugada(0, 0, 2), "Casilla ya con bandera, no se debe eliminar la bandera");
+
+
     // CASO DONDE SE QUITA LA BANDERA DE UNA CASILLA CON BANDERA
     assertTrue(gestor.realizar_jugada(0, 0, 3), "No deberia tener bandera, ya que se ha quitado");
+
+
+    //CASO DONDE SE QUITA UNA BANDERA DONDE NO LA HAY
+    assertFalse(gestor.realizar_jugada(2, 2, 3), "No deberia tener bandera");
+
+    //CASO QUITAR FLAG EN CASILLA REVELADA (1,1) está revelada de antes
+    assertFalse(gestor.realizar_jugada(1, 1, 3), "No deberia quitar la bandera de una celda ya revelada");
 
     //Quitar bandera de posicion no válida
     assertFalse(gestor.realizar_jugada(-1, -1, 3), "No deberia tener bandera, ya que se ha quitado");
