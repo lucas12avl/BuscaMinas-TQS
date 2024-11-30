d0f6152b7d8dc0746ac5537df763991ac2d76e2f
alma <alva2003ro@gmail.com>
2024-11-30 09:32:21 +0100

    TEST GET INT EMPEZADO (ENTRADA DE USUARIO)

--- src/test/java/controlador/InputUserTest.java
+++ src/test/java/controlador/InputUserTest.java
@@ -1,5 +1,18 @@
 package controlador;
+package controlador;
+import model.Casilla;
+import model.Tablero;
+import org.junit.jupiter.api.Test;
+
+
+
+
 //Tests de input de l'usuari
 public class InputUserTest {
+
+  @Test
+  void testGetInt(){
+
+  }
 
 }
